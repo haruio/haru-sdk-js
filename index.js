0cf9fcbecacdf0ce7b471232bb88dd4a86ea3c76
jungun.park <jungun.park@makeus.com>
2015-11-18 21:28:40 +0900

    entity 에 대한 함수 정

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function extend(target, source) {
-  if (source) {
-    for ( var key in source) {
-      var val = source[key];
-      if ( typeof val !== "undefined") {
-        target[key] = val;
-      }
-    }
-  }
-  return target;
-}
+(function(){
+	function extend(target, source) {
+	  if (source) {
+	    for ( var key in source) {
+	      var val = source[key];
+	      if ( typeof val !== "undefined") {
+	        target[key] = val;
+	      }
+	    }
+	  }
+	  return target;
+	}
+
+	function getEntity(){
+
+	}
+	function editEntity(){
+
+	}
+
+
+})
